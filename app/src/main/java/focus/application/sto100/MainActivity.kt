a22f1486bf31a03c165075d0d07203ecce03831a
Aleksandr <alexandrguap@gmail.com>
2019-06-30 23:16:46 +0500

    first commit

---
+++ app/src/main/java/focus/application/sto100/MainActivity.kt
@@ -0,0 +1,20 @@
+package focus.application.sto100
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import focus.application.sto100.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+
+
+
+}
